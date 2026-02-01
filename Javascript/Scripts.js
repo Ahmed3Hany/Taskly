cf6d2a404640c04fab5744fac8d3220118876962
Ahmed Hany <142047937+Ahmed3Hany@users.noreply.github.com>
2026-02-01 03:55:29 +0200

    Add files via upload

--- Javascript/Scripts.js
+++ Javascript/Scripts.js
@@ -1,16 +1,19 @@
 'use strict';
+//Fixing Browser Error
+document.addEventListener("DOMContentLoaded", function () {
+    // Enable tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+});
 
 // Hero Section Height Adjustment
 function heroSectionHeight() {
     const nav = document.getElementById('navbar');
     const navHeight = nav.offsetHeight;
     const heroSection = document.querySelector('.hero-section');
-    if(window.innerHeight >= 700){
-        heroSection.style.minHeight = `calc(100vh - ${navHeight}px - 15px) `;
-    }
-    else{
-        heroSection.style.minHeight = `100vh`;
-    }
+    heroSection.style.minHeight = `calc(100vh - ${navHeight}px - 15px) `;
 }
 
 // Dark Mode Toggle with State Persistence
@@ -24,45 +27,75 @@ checkbox.addEventListener("change", () => {
 });
 
 // Responsive Data Forms Display
-const overlay = document.getElementById('overlay');
-function dataFormsStartup(){
+let signUpShown = false;
+function dataFormsStartup() {
+    const showbtn = document.getElementById('showSignUp');
     const loginForm = document.getElementById('login');
     const registerForm = document.getElementById('register');
-    if(window.innerWidth < 768){
-        registerForm.style.display = "none";
-        overlay.style.display = "none";
-        loginForm.style.display = "flex";
+    if (window.innerWidth < 768) {
+        registerForm.style.position = "absolute";
+        registerForm.style.top = `-${registerForm.offsetHeight + 20}px`;
+        registerForm.style.left = `0px`;
+        loginForm.style.position = "relative";
+        loginForm.style.top = `0px`;
+        showbtn.style.top = `-85px`;
+        signUpShown = false;
+    }else{
+        registerForm.style.position = "relative";
+        registerForm.style.top = `0px`;
+        loginForm.style.position = "relative";
+        loginForm.style.top = `0px`;
+        showbtn.style.top = `-85px`;
+        signUpShown = false;
     }
-    else{
-        registerForm.style.display = "flex";
-        loginForm.style.display = "flex";
-        overlay.style.display = "flex";
+}
+
+function showSignUpForm() {    
+    const loginForm = document.getElementById('login');
+    const showbtn = document.getElementById('showSignUp');
+    const registerForm = document.getElementById('register');
+    if (signUpShown === false) {
+        registerForm.style.position = "relative";
+        registerForm.style.top = `0px`;
+        loginForm.style.position = "absolute";
+        loginForm.style.top = `${registerForm.offsetHeight + 20}px`;
+        showbtn.style.top = `${loginForm.offsetHeight - 85}px`;
+        signUpShown = true;
+    }
+    else {
+        loginForm.style.position = "relative";
+        loginForm.style.top = `0px`;
+        registerForm.style.position = "absolute";
+        registerForm.style.top = `-${loginForm.offsetHeight + 20}px`;
+        showbtn.style.top = `-85px`;
+        signUpShown = false;
     }
 }
 
 let isLoginVisible = true;
-function toggleDataForms(){
-    if(isLoginVisible){
+function toggleDataForms() {
+    const overlay = document.getElementById('overlay');
+    if (isLoginVisible) {
         overlay.innerHTML = `<h1>Hello, Friend!</h1>
         <p>Dont Have an Account? Sign Up Now!</p>
         <button class="submitBtn" onclick="toggleDataForms();" value="SignUP">Sign UP</button>`;
         overlay.style.right = `0px`;
-        overlay.style.backgroundPosition = 'right';
+        overlay.style.setProperty('--bg-pos', 'right');
         isLoginVisible = false;
     }
-    else{
+    else {
         overlay.innerHTML = `<h1>Welcome Back!</h1>
         <p>Already Have an Account? Sign In Now!</p>
         <button class="submitBtn" onclick="toggleDataForms();" value="SignIN">Sign IN</button>`;
         overlay.style.right = `${overlay.offsetWidth}px`;
-        overlay.style.backgroundPosition = 'left';
+        overlay.style.setProperty('--bg-pos', 'left');
         isLoginVisible = true;
     }
 }
 
-// function adjustDataFormsOnResize(){
-//     if(isLoginVisible == true){
-//         console.log('resize detected');
-//         overlay.style.right = `${overlay.offsetWidth}px`;
-//     }
-// }
+function adjustDataFormsOnResize(){
+    if(isLoginVisible == true){
+        console.log('resize detected');
+        overlay.style.right = `${overlay.offsetWidth}px`;
+    }
+}
